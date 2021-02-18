0f92cc1c26c1b49dadb6d4dbb08cc0b23dfb620a
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-02-18 15:38:11 +0100

    initial commit

---
+++ APIklient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace APIklient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+            RestRequest request = new RestRequest("pokemon/ditto");
+            IRestResponse response = client.Get(request);
+            Console.WriteLine(response.Content);
+
+        }
+    }
+}
