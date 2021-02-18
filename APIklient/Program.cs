83b4cbbda933a385847058cf818161c6f210be3d
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-02-18 16:33:28 +0100

    anslöt till en annan api men förstod inte riktigt hur man sökte i apin även om jag läste en massa på deras sida :(

--- APIklient/Program.cs
+++ APIklient/Program.cs
@@ -8,14 +8,13 @@ namespace APIklient
     {
         static void Main(string[] args)
         {
-            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
-            RestRequest request = new RestRequest("pokemon/ditto");
+            RestClient client = new RestClient("http://stapi.co/api/v1/rest/season/search");
+            RestRequest request = new RestRequest("");
             IRestResponse response = client.Get(request);
             Console.WriteLine(response.Content);
             System.Console.ReadLine();
 
             Pokemon newPokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content);
-
         }
     }
 }
